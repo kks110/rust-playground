e6fbf9b3f2e1611f167a52c4f31acfca99518da3
Kelvin Samuel <kelvin@kks110.com>
2020-09-01 23:33:20 +0100

    playing with structs

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,46 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other_rectangle: &Rectangle) -> bool {
+        self.height > other_rectangle.height && self.width > other_rectangle.width
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    let sq1 = Rectangle::square(10);
+    println!("The area of Square 1 is {}", sq1.area());
+
+    println!("rect1 is {:#?}", rect1);
+
+    println!("The area of rectangle 1 is {} square pixels.", rect1.area());
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+D:\code\rust\rust-playground\rectangles\target\debug\rectangles.exe: D:\code\rust\rust-playground\rectangles\src\main.rs
